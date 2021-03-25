42d1028b2f22458eb132bae196f3b7256f4c2173
MK-D-Luffy <1905470291@qq.com>
2021-03-25 21:29:19 +0800

    修改

--- DataStructures/src/main/java/com/study/sort/QuickSort.java
+++ DataStructures/src/main/java/com/study/sort/QuickSort.java
@@ -1,6 +1,5 @@
 package com.study.sort;
 
-
 import java.util.Random;
 
 /**
@@ -22,60 +21,63 @@ public class QuickSort {
         // 快速排序
         quickSort(arr, 0, arr.length - 1);
         long end = System.currentTimeMillis();
-        System.out.println("排序共消耗了 " + (end - start) + " ms"); // 8w:32ms,800w:1s
+        System.out.println("排序共消耗了 " + (end - start) + " ms"); // 8w:16ms,800w:720ms
         System.out.println("end");
 
     }
 
-    public static void quickSort(int[] arr, int left, int right) {
-        // 分区
-        int temp;
-        int l = left; // 左下标
-        int r = right; // 右下标
-        // 中轴值
-        int pivot = arr[(left + right) / 2];
-        while (l < r) {
-            // 找到左边比大于等于pivot的数
-            while (arr[l] < pivot) {
-                l++;
-            }
-            // 找到右边比小于等于pivot的数
-            while (arr[r] > pivot) {
-                r--;
-            }
-            // 如果 l>=r 说明两边已经遍历完了,没有找到符合条件的数,说明已经有序,则退出while循环.
-            if (l >= r) {
-                break;
-            }
-            // 如果没退出,则进行交换
-            temp = arr[l];
-            arr[l] = arr[r];
-            arr[r] = temp;
+    // 快速排序
+    public static void quickSort(int[] arr, int lo, int hi) {
+        if (hi <= lo) {
+            return;
+        }
+        int j = partition(arr, lo, hi);    // 切分
+        quickSort(arr, lo, j - 1);      // 将左半部分排序
+        quickSort(arr, j + 1, hi);      // 将右半部分排序
 
-            // 如果交换完后满足 arr[l] == pivot 说明r的右边已经都是符合条件的了,
-            // 此时[l,r)之间的数还没有遍历到,所以将r左移,继续循环,直到遍历完,退出程序.
-            if (arr[l] == pivot) {
-                r--;
-            }
-            // 如果交换完后满足 arr[r] == pivot 说明l的左边已经都是符合条件的了,
-            // 此时(l,r]之间的数还没有遍历到,所以将l右移,继续循环,直到遍历完,退出程序.
-            if (arr[r] == pivot) {
-                l++;
-            }
-        }
-
-        if (l == r) {
-            l++;
-            r--;
-        }
-        // 向左递归
-        if (left < r) {
-            quickSort(arr, left, r);
-        }
-        // 向右递归
-        if (l < right) {
-            quickSort(arr, l, right);
-        }
     }
 
+    // 快速排序的切分
+    public static int partition(int[] arr, int lo, int hi) {
+        // 将数组切分伟a[lo...i-1],a[i],a[i+1...hi]
+        int i = lo, j = hi + 1; // 左右扫描指针
+        int v = arr[lo];        // 切分元素
+        while (true) {
+            // 先将i后移再比较,是因为基准值设在了第一个
+            while (arr[++i] < v) {
+                // 从左向右扫描,扫描到数组最后一个,说明数组已经全部有序,则退出
+                if (i == hi) {
+                    break;
+                }
+            }
+            // 先将j前移再比较,是因为j在初始化的时候是最后一个的后一个
+            while (arr[--j] > v) {
+                // 从右向左扫描,扫描到数组第一个,说明数组已经全部有序,则退出
+                if (lo == j) {
+                    break;
+                }
+            }
+            // 左右扫描后(left,i)和(j,right)已经有序,
+            // 只要再将已经有序中间位置与基准位置交换就行了.
+            if (i >= j) {
+                break;
+            }
+            swap(arr, i, j);
+        }
+        // 循环结束说明:
+        // 在最后一次循环中j会前移,i会后移,
+        // j前移后还是小于基准值的,而i后移会变成大于基准值的一部分
+        // 所以选j不选i
+        swap(arr, lo, j);
+
+        return j;
+    }
+
+    // 通过下标交换数组的元素
+    public static void swap(int[] arr, int i, int j) {
+        int temp;
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
